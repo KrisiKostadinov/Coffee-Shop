e93562e13b58f3c8cd9fe7be791ce5458d9f3dbf
krisikostadinov <krisi.199898@gmail.com>
2023-01-22 17:05:23 +0200

    Add deleteing session.

--- users/logout.php
+++ users/logout.php
@@ -2,10 +2,14 @@
 
 session_start();
 include_once("../functions.php");
+include_once("../dbconn.php");
 
 if ( is_authenticated() ) {
+    $id = $_SESSION["user"]["id"];
     unset($_SESSION["access_token"]);
     unset($_SESSION["access_token_expity"]);
     unset($_SESSION["user"]);
+    $query = "DELETE FROM sessions WHERE user_id = $id";
+    $query_run = mysqli_query($conn, $query);
     redirect("users/login");
 }
