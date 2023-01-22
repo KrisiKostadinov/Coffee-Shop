b90e45398f0261afbb241af0c8e989296f81e034
krisikostadinov <krisi.199898@gmail.com>
2023-01-22 13:58:01 +0200

    Initial commit.

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php $meta_title = "404 Not Found" ?>
+<?php include_once("inc/header.php") ?>
+<?php include_once("dbconn.php") ?>
+<main>
+    <h1 class="text-center text-4xl mt-4">404 Not Found</h1>
+</main>
+<?php include_once("inc/footer.php") ?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php $meta_title = "Coffee Ordering System" ?>
+<?php $meta_description = "Welcome to coffee ordering system" ?>
+
+<?php
+session_start();
+include_once("functions.php");
+
+include_once("inc/header.php");
+include_once("dbconn.php");
+
+include_once("inc/navbar.php") ?>
+
+<main>
+    <div class="grid grid-cols-1 gap-4">
+        <h1 class="text-center text-4xl mt-4">Welcome to coffee ordering system</h1>
+        <?php include_once("inc/home/menu.php") ?>
+        <div class="container mx-auto">
+            <?php include_once("inc/message.php") ?>
+        </div>
+        <?php include_once("inc/home/order.php") ?>
+    </div>
+</main>
+
+<?php include_once("inc/footer.php") ?>
